383f081c57b7010be41e7e9224e40e634c78bf00
Jonas Egetoft Knudsen <jonkn0001@elevcampus.dk>
2024-02-08 11:01:53 +0100

    user pages

--- Flockbuster/Pages/User/Dashboard.cshtml.cs
+++ Flockbuster/Pages/User/Dashboard.cshtml.cs
@@ -20,33 +20,37 @@ namespace Flockbuster.Pages.User
 		[BindProperty]
 		public Users UserDetails { get; set; }
 		[BindProperty]
-		public List<BorrowedMovie> ListOfMovies { get; set; } = new List<BorrowedMovie>();
+		public List<Movies> ListOfMovies { get; set; } = new List<Movies>();
 		public IActionResult OnGet()
-        {
-            if (!HttpContext.Session.GetInt32("ID").HasValue || HttpContext.Session.GetBoolean("Admin"))
-            {
-                return RedirectToPage("/error/403");
-            }
-            else
-            {
-                int? tempId = HttpContext.Session.GetInt32("ID");
-                int id = tempId.HasValue ? tempId.Value : 0;
-                UserDetails = _user.GetUsersById(id);
-                BorrowedMovie loanedMovie = new BorrowedMovie();
-                loanedMovie = _movie.GetBorrowedMovieById(id);
+		{
+			if (!HttpContext.Session.GetInt32("ID").HasValue || HttpContext.Session.GetBoolean("Admin"))
+			{
+				return RedirectToPage("/error/403");
+			}
+			else
+			{
+				int? tempId = HttpContext.Session.GetInt32("ID");
+				int id = tempId.HasValue ? tempId.Value : 0;
+				UserDetails = _user.GetUsersById(id);
 
-                if (loanedMovie.Id != 0 && loanedMovie.IsBorrowed)
-                {
-                    ListOfMovies.Add(loanedMovie);
-                }
-                foreach (BorrowedMovie mov in ListOfMovies)
-                {
-					List<Genre> genresForMovie = _movie.GetMovieGenre(id);
-					mov.BorrowedMoviesGenres = genresForMovie;
+				BorrowedMovie loanedMovie = new BorrowedMovie();
+				loanedMovie = _movie.GetBorrowedMovieById(id);
+
+				Movies borrowedMovie = new Movies();
+				borrowedMovie = _movie.GetMoviesById(loanedMovie.MovieID);
+
+				if (loanedMovie.Id != 0 && loanedMovie.IsBorrowed)
+				{
+					ListOfMovies.Add(borrowedMovie);
+				}
+				foreach (Movies mov in ListOfMovies)
+				{
+					List<Genre> genresForMovie = _movie.GetMovieGenre(loanedMovie.MovieID);
+					mov.Genres = genresForMovie;
 				}
 
-                return Page();
-            }
-        }
-    }
+				return Page();
+			}
+		}
+	}
 }
--- Flockbuster/Pages/User/MovieHistory.cshtml.cs
+++ Flockbuster/Pages/User/MovieHistory.cshtml.cs
@@ -1,4 +1,5 @@
 using Flockbuster.Domain.Models;
+using Flockbuster.Service.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using static System.Reflection.Metadata.BlobBuilder;
@@ -7,16 +8,48 @@ namespace Flockbuster.Pages.User
 {
     public class MovieHistoryModel : PageModel
     {
-        public IActionResult OnGet()
-        {
-            if (!HttpContext.Session.GetInt32("ID").HasValue || HttpContext.Session.GetBoolean("Admin"))
-            {
-                return RedirectToPage("/errors/403");
-            }
-            else
-            {
-                return Page();
-            }
-        }
-    }
+		private readonly IUser _user;
+		private readonly IMovie _movie;
+
+		public MovieHistoryModel(IUser user, IMovie movie)
+		{
+			_user = user;
+			_movie = movie;
+		}
+		[BindProperty]
+		public Users UserDetails { get; set; }
+		[BindProperty]
+		public List<Movies> ListOfMovies { get; set; } = new List<Movies>();
+		public IActionResult OnGet()
+		{
+			if (!HttpContext.Session.GetInt32("ID").HasValue || HttpContext.Session.GetBoolean("Admin"))
+			{
+				return RedirectToPage("/error/403");
+			}
+			else
+			{
+				int? tempId = HttpContext.Session.GetInt32("ID");
+				int id = tempId.HasValue ? tempId.Value : 0;
+				UserDetails = _user.GetUsersById(id);
+
+				BorrowedMovie loanedMovie = new BorrowedMovie();
+				loanedMovie = _movie.GetBorrowedMovieById(id);
+
+				Movies borrowedMovie = new Movies();
+				borrowedMovie = _movie.GetMoviesById(loanedMovie.MovieID);
+
+				if (loanedMovie.Id != 0 && loanedMovie.WasBorrowed)
+				{
+					ListOfMovies.Add(borrowedMovie);
+				}
+				foreach (Movies mov in ListOfMovies)
+				{
+					List<Genre> genresForMovie = _movie.GetMovieGenre(loanedMovie.MovieID);
+					mov.Genres = genresForMovie;
+				}
+
+				return Page();
+			}
+		}
+	}
 }
