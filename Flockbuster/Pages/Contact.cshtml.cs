7c03ff2502e6a65e356ab76d9a73805e8ffb83e8
Jonas Egetoft Knudsen <jonkn0001@elevcampus.dk>
2024-02-23 12:50:13 +0100

    ...

--- Flockbuster/Pages/Contact.cshtml.cs
+++ Flockbuster/Pages/Contact.cshtml.cs
@@ -1,12 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace Flockbuster.Pages
 {
     public class ContactModel : PageModel
     {
+        [BindProperty]
+        public string FullName { get; set; } = string.Empty;
+
+        [BindProperty]
+        public string Email { get; set; } = string.Empty;
+
+        [BindProperty]
+        public string Subject { get; set; } = string.Empty;
+
+        [BindProperty, MaxLength(500)]
+        public string Textarea { get; set; } = string.Empty;
+
         public void OnGet()
         {
+        }
+        public IActionResult OnPostContact()
+        {
+            return Page();
         }
     }
 }
